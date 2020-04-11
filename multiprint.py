d30e9080d40f62f0c374064e9f5181cad2fda6ce
jesenator <32143901+jesenator@users.noreply.github.com>
2020-04-11 15:30:38 -0400

    Update multiprint.py

--- multiprint.py
+++ multiprint.py
@@ -1,10 +1,12 @@
-######################################################################### variables
+######################################################################### variable declerations
 line=["G1 X0.1 Y20 Z0.3 F5000.0 ; Move to start position\nG1 X0.1 Y200.0 Z0.3 F1500.0 E15 ; Draw the first line",
       "G1 X0.4 Y200.0 Z0.3 F5000.0 ; Move to side a little\nG1 X0.4 Y20 Z0.3 F1500.0 E30 ; Draw the second line"]
+
 newLine=["",""]
+
 subgcode=''';-----------------start of multiprint substitution-----------------
-G1 X110.452134 Y220 F8000; Move to the back
-G1 Z0 F8000; Lower
+G1 X110.452134 Y220 F8000; Move to the back (note that the avg x value is substituted)
+G1 Z0 F8000; Lower so nozel is pressed into bed and filament cannot leak out
 
 ;no bed cool
 
@@ -17,12 +19,9 @@ G1 Y20 F8000; shake
 G1 Y0 F8000; shake
 G0 Y190;  move back
 G0 Y1;  move forward to expose sd card slot
+;-------------------------------------------------------------------------------
+'''
 
-M300 S3520 P200 ; A7
-M300 S4698.63 P200 ; D8
-M300 S5274.04 P200 ; E8
-M300 S6271.93 P200 ; G8
-;-------------------------------------------------------------------------------'''
 maskSubgcode='''
 ;----COVID mask removal substitution------
 G1 Z4 F8000
@@ -38,9 +37,11 @@ G1 Y140 F8000; go backward
 G1 Z4; go down
 ;-----------------------------------------
 '''
+
 shieldSubgcode='''
 ;--------COVID face shield removal substition-------------
-G1 X235 Y150 F10000
+G1 Z20 F10000
+G1 X235 Y150 
 G1 Z.2
 G1 X230 F1000
 G1 X231
@@ -54,18 +55,6 @@ G1 X115
 G1 Z.2
 G1 Y0 F1000
 ;------------------------------------------------
-
-'''
-
-
-'''
-reset:
-
-G1 Z60 F10000
-
-G1 X220 Y220 F8000; Move to the back
-
-
 '''
 
 toDelete='''
@@ -81,31 +70,39 @@ M104 S0
 '''
 
 bedCool='''
-;M190 R40; wait for bed to reach exactly 40
 M190 S0; set bed to 0
 G4 S1; wait
 '''
 
 offset=1.6
-#########################################################################inputs
+fileLocation="C:/Users/lgilb/AppData/Local/Programs/Python/Python38/G code/"
+######################################################################### user inputs
+filename="CE3_NIH visor.gcode"
+n=10
 coolTime=0
-COVID="none"
-filename=input("paste the filename of the gcode to be edited")
-n=int(input("enter the number of objects needed"))
+COVID="shield"
+#the file needs to be in the specified folder and not be labled with .gcode, but when typing the name it should have the .gcode
+
+#uncomment the follwing 4 lines to be able to enter their valus
+
+filename=input("paste the filename of the gcode to be edited (make sure it is in the g code folder")
+#n=int(input("enter the number of objects needed"))
 #coolTime=float(input("how long should the bed wait to cool down (minutes) "))
-COVID=input("COVID mask removal sequence? (mask or shield or none)")
+#COVID=input("COVID mask removal sequence? (mask or shield or none)")
+
 if(COVID=="mask"):
     print("note: ensure that the mask is in the correct spot with Y=27.5295, and the nose pointing in positive Y direction")
 if(COVID=="shield"):
-    print("note: ensure that the face shield is in the correct spot")
+    print("note: ensure that the face shield is in the correct spot Y=28 X=-3")
 
-gcode = open(filename, "r+", 1)
+#read file
+gcode = open((fileLocation+filename), "r+", 1)
 originalCode=gcode.read()
 newCode=originalCode.replace(toDelete, "")
 
 
-linesx=.4+offset*(n-1)
 
+#read values from g code such as filament used, time, and min and max location values
 gcode.seek(0,0)
 gcode.readline()
 time=gcode.readline()
@@ -119,12 +116,16 @@ gcode.readline()
 maxx=gcode.readline()
 maxy=gcode.readline()
 
+meters=float(meters[16:-2])*n
 minx=float(minx[6:-1])
 maxx=float(maxx[6:-1])
 maxy=float(maxy[6:-1])
 avgx=round((minx+maxx)/2, 1)
 subgcode=subgcode.replace("110.452134", str(avgx))
 
+#adding in more g code to the subtitution for more features
+#like a special removal sequence for the COVID-19 respirators and face shields
+#which have too much surface area to be jsut pushed off
 if(coolTime>0):
     bedCool=bedCool.replace("S1", ("S" + str(coolTime*60)))
     subgcode=subgcode.replace(";no bed cool", bedCool)
@@ -132,39 +133,39 @@ if(COVID=="mask"):
     subgcode=subgcode.replace(";no Covid special removal", maskSubgcode)
 if(COVID=="shield"):
            subgcode=subgcode.replace(";no Covid special removal", shieldSubgcode)
-     
 
+#figuring out how far the purge lines will go based on how many times it is iterated
+linesx=.4+offset*(n-1)     
 maxObjects=round(((-.4+minx)/offset))
 if(n>maxObjects):
     print("ERROR: the lines will overlap with the print. (they go to " + str(linesx) + ") Either print is too close to left edge, or it is iterated too many times")
     print("it will automatically set to the maximum possible objects")
     n=maxObjects
 
-meters=float(meters[16:-2])*n
 
 filename=filename.replace(".gcode", "") + "[x" + str(n) + "].gcode"
-newgcode=open(filename, "w+")
+newgcode=open((fileLocation+filename), "w+")
 
 
 gcode.seek(0,0)
 
 for a in range(0, int(n)-1):
     code=originalCode
+    #replace x values of the purgeline over by an offset for each iteration
     newLine[0]=line[0].replace("X0.1", ("X" + str(round((0.1+offset*int(a+1)), 1)))) 
     newLine[1]=line[1].replace("X0.4", ("X" + str(round((0.4+offset*int(a+1)), 1))))
-
     code=code.replace(line[0], newLine[0])
     code=code.replace(line[1], newLine[1])
 
-    if(a<n-2):
+    if(a<n-2): #for each itiration except the last, remove the g code that tells the printer to turn off the bed and hot end
         code=code.replace(toDelete, "")
     
     newCode=newCode + subgcode + "\n;--------start of iteration number " + str(a+2) + "--------\n" + code
-#print(newCode)
-#newgcode.write(subgcode)
+
+#write new G code to the file
 newgcode.write(newCode)
 
-
+#print info
 print("\nfilename: " + filename)
 print("total time: " + str(round((int(time[6:])+30)*n/3600, 2)) + " hours")
 print("total meters: " + str(round(meters, 2)))
@@ -181,23 +182,16 @@ add more post processing funcitons
     -some way to get prints off the build plate easier and reduce damage to gantry
     -reduce chance of failure
     -use it to print a lot of something to test it
-
 things to improve removal
     
-
 things to fix
     -for some reason, the empty_Multiprint(n).gcode is empty when n is <=4 i have no idea why.
         also, the newgcode write isn't working how i expect it to
-
     -after printing in the same place repeatedly, the print sticks much better, which makes it harder to remove
         -have the mask be print in different places
-
+    -make the notes actually work
 files
     CE3_covid_mask(.6mm).gcode - messed up because layers delaminated when removing the print, so it didnt come all the way off
     CE3_mask(.6~.4~210-105).gcode - .4 layer heigh instead of .48
-
-
 '''
-
-
 
